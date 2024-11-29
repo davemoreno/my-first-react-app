4411056b295861b57c440d6e74cdfbceaa83aaf9
David Moreno <info@davidmoreno.dev>
2024-11-29 08:19:52 +0100

    What is JSX?

--- src/Saludos.jsx
+++ src/Saludos.jsx
@@ -1,5 +1,16 @@
 function Saludos() {
-    return <h1>&quot;Estos son mis saludos&quot;</h1>;
+    return (
+      <div className="container">
+        <svg>
+          <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+        </svg>
+        <h1>&quot;Estos son mis saludos&quot;</h1>
+        <p>El parrafo</p>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    );
   }
   
   export default Saludos;
